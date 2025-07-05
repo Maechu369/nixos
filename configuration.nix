4e318b25f7f639f1c08782152fa770cc590caadc
Maechu369 <90904222+Maechu369@users.noreply.github.com>
2025-07-05 16:36:22 +0900

    nix install iso image

---
+++ configuration.nix
@@ -0,0 +1,42 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports = [
+    (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix")
+  ];
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  time.timeZone = "Asia/Tokyo";
+  networking.hostName = "nixos";
+
+  # Pick ONLY ONE of the below networking options.
+  # And, NetworkManager is recommended, wireless was deprecated.
+  networking.wireless.enable = false;
+  networking.networkmanager.enable = true;
+  # ENDPICK
+
+
+  users.users.hiroki = {
+      isNormalUser = true;
+      home = "/home/hiroki";
+      extraGroups = ["wheel" "networkmanager"];
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+  environment.systemPackages = with pkgs;[
+    git
+    networkmanager
+  ];
+
+  services.openssh.enable = true;
+}
---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "Minimal NixOS installation media";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations = {
+      exampleIso = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
