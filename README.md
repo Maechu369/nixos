78f97855e45568bda440b6fe9de6a3dd4d560fed
Maechu369 <90904222+Maechu369@users.noreply.github.com>
2025-07-07 22:15:20 +0900

    add letsnote

---
+++ README.md
@@ -0,0 +1,8 @@
+# NixOS Configs
+
+This repository is all of my nix environments.
+
+./directoryName is a environment.
+
+- wsl2
+- letsnote
---
+++ letsnote/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "NixOS config";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=release-25.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {self, nixpkgs, home-manager, ...}@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.hiroki = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
