02c92fdd2baeaf0e081fc8ce26a5d65b4c4335b1
Maechu369 <90904222+Maechu369@users.noreply.github.com>
2025-07-10 18:31:59 +0900

    purge zsh

--- component/programs.nix
+++ component/programs.nix
@@ -15,55 +15,7 @@
       enable = true;
     };
   };
-  zsh = {
-    enable = true;
-    dotDir = ".config/zsh";
-    autocd = true;
-    autosuggestion.enable = true;
-    cdpath = ["~" ".."];
-    dirHashes = {
-      c = "/mnt/c";
-      wh = "/mnt/c/Users/hiroki";
-    };
-    history = {
-      ignoreAllDups = true;
-      ignoreSpace = true;
-    };
-    profileExtra = "test -f $HOME/.profile && . $HOME/.profile";
-    sessionVariables = {
-      TERM = "xterm-256color";
-      EDITOR = "nvim";
-      EZA_ICON_SPACING = 1;
-    };
-    initContent = "";
-    shellAliases = {
-      "..." = "cd ../..";
-      "...." = "cd ../../..";
-      zmv = "noglob zmv -W";
-      q = "exit";
-      c = "cd";
-      v = "vim";
-      vim = "nvim";
-      python = "python3";
-      py = "python";
-      l = "ls";
-      ls = "eza -F";
-      la = "eza -aF";
-      ll = "eza -aahlF";
-      lt = "eza -aT -L 3 -I '.git|.cache'";
-      diff = "delta";
-      diffs = "DELTA_FEATURES=+side-by-side delta";
-      gt = "cd `git rev-parse --show-toplevel`";
-      gs = "git status";
-      gl = "git log --all --graph";
-      gitc = "git checkout";
-      gls = "git ls-files";
-      clean = "git clean -ifd";
-      commit = "git commit -m";
-      pc = "procs --tree";
-      lsblk = "lsblk -ipo +UUID";
-    };
-  };
+  zsh = import zsh/zsh.nix;
   eza = {
     enable = true;
     git = true;
