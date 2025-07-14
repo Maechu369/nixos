a6f266104ae3b45607c792f7887ea80713ea5212
Maechu369 <90904222+Maechu369@users.noreply.github.com>
2025-07-14 22:23:39 +0900

    add git fzf boost

--- component/zsh/default.zsh
+++ component/zsh/default.zsh
@@ -11,3 +11,36 @@ bindkey "^[[1;5C" forward-word
 bindkey "^[[1;5D" backward-word
 bindkey "^[[3~" delete-char
 
+git() {
+  if [[ $1 == 'diffs' ]]; then
+    if [[ $(command -v delta) ]]; then
+      DELTA_FEATURES=+side-by-side
+      command git diff "${@:2}"
+      DELTA_FEATURES=+
+    else
+      command git "$@"
+    fi
+  elif [[ $1 == 'stash' ]]; then
+    if [[ $2 == 'list' ]]; then
+      if [[ $(command -v fzf) ]]; then
+        local stash
+        DELTA_FEATURES=+side-by-side
+        stash=$(command git stash list | awk -F'[: ]' '{print $1}' | fzf --prompt='git stash > ' --preview='command git stash list | grep {}; command git stash show -p {}')
+        if [[ $stash == '' ]]; then
+          DELTA_FEATURES=+
+          return
+        fi
+        git stash show -p "$stash" >&2
+        echo "$stash"
+        DELTA_FEATURES=+
+      else
+        command git "$@"
+      fi
+    else
+      command git "$@"
+    fi
+  else
+    command git "$@"
+  fi
+}
+
