fd54cbeea19bb5d6ecce09134e9b6ed69fccac16
Maechu369 <90904222+Maechu369@users.noreply.github.com>
2025-07-19 22:46:54 +0900

    set home-manager backup

--- letsnote/flake.nix
+++ letsnote/flake.nix
@@ -3,6 +3,7 @@
   inputs = {
     # nixpkgs.url = "github:nixos/nixpkgs?ref=release-25.05";
     nixpkgs.url = "github:nixos/nixpkgs/29e290002bfff26af1db6f64d070698019460302";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -19,6 +20,7 @@
       modules = [
         ./config.nix
         home-manager.nixosModules.home-manager {
+          home-manager.backupFileExtension = "backup";
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.hiroki = import ./home.nix;
