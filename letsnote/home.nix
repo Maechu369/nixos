414ef886c405d03cdcea50ec6c712d1c4c4d0130
Maechu369 <90904222+Maechu369@users.noreply.github.com>
2025-08-03 00:26:47 +0900

    add obsidian

--- letsnote/home.nix
+++ letsnote/home.nix
@@ -4,7 +4,8 @@ args@{ config, pkgs, plasma-manager, nixvim, ... }:
   home.username = "hiroki";
   home.homeDirectory = "/home/hiroki";
   home.stateVersion = "25.05";
-  home.packages = (import ../component/packages.nix args);
+  home.packages = (import ../component/packages.nix args)
+    ++ (import ../component/desktop/packages.nix args);
   xdg.configFile = {
     fcitx5.source = ./fcitx5;
     fcitx5.recursive = true;
