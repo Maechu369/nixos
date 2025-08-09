06276c2c8bba4b0195e2e837800ca9db109cb765
Maechu369 <90904222+Maechu369@users.noreply.github.com>
2025-08-09 23:44:42 +0900

    add mypc

---
+++ mypc/config.nix
@@ -0,0 +1,246 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, lib, pkgs, username, nixos-hardware, xremap, ... }:
+let ageKeyFile = "/var/lib/sops-nix/keys.txt";
+in {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ] ++ [ xremap.nixosModules.default ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 32;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Tokyo";
+
+  # Select internationalisation properties.
+  i18n = {
+    defaultLocale = "ja_JP.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "ja_JP.UTF-8";
+      LC_IDENTIFICATION = "ja_JP.UTF-8";
+      LC_MEASUREMENT = "ja_JP.UTF-8";
+      LC_MONETARY = "ja_JP.UTF-8";
+      LC_NAME = "ja_JP.UTF-8";
+      LC_NUMERIC = "ja_JP.UTF-8";
+      LC_PAPER = "ja_JP.UTF-8";
+      LC_TELEPHONE = "ja_JP.UTF-8";
+      LC_TIME = "ja_JP.UTF-8";
+    };
+    inputMethod = {
+      enable = true;
+      type = "fcitx5";
+      fcitx5 = {
+        addons = with pkgs; [ fcitx5-mozc fcitx5-skk ];
+        waylandFrontend = true;
+        settings.inputMethod = {
+          Wayland."InputMethod[$e]" =
+            "/run/current-system/sw/share/applications/org.fcitx.Fcitx5.desktop";
+        };
+      };
+    };
+  };
+  services.dbus.packages = [ config.i18n.inputMethod.package ];
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  fonts = {
+    packages = with pkgs; [
+      noto-fonts-cjk-serif
+      noto-fonts-cjk-sans
+      noto-fonts-emoji
+      plemoljp-nf
+    ];
+    fontDir.enable = true;
+    fontconfig = {
+      defaultFonts = {
+        serif = [ "Noto Serif CJK JP" "Noto Color Emoji" ];
+        sansSerif = [ "Noto Sans CJK JP" "Noto Color Emoji" ];
+        monospace = [ "PlemolJP Console NF" ];
+        emoji = [ "Noto Color Emoji" ];
+      };
+    };
+  };
+
+  sops = {
+    age.keyFile = ageKeyFile;
+    age.generateKey = true;
+    defaultSopsFile = ../secrets/secret.yaml;
+    defaultSopsFormat = "yaml";
+    secrets.hashedPassword.neededForUsers = true;
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "jp";
+    variant = "";
+  };
+
+  # environment.etc."skel/.config/kxkbrc".text = builtins.readFile ./kxkbrc;
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  users.users."${username}" = {
+    isNormalUser = true;
+    description = username;
+    hashedPasswordFile = config.sops.secrets.hashedPassword.path;
+    extraGroups = [ "networkmanager" "wheel" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILv9JtMAq/KexNVaWmvyh7ouppoA0aDPO8qxlnYUQDtq ${username}@nixos"
+    ];
+    shell = pkgs.zsh;
+    packages = with pkgs; [ kdePackages.kate ];
+  };
+  users.mutableUsers = false;
+  security.sudo.wheelNeedsPassword = false;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [ ];
+
+  environment.variables = {
+    SOPS_AGE_KEY_FILE = ageKeyFile;
+    XKB_CONFIG_ROOT = "${pkgs.xkeyboard_config}/share/X11/xkb";
+  };
+
+  programs.zsh.enable = true;
+  programs.steam = {
+    # 設定は左上のSteamアイコンから開くことができる。
+    # 言語設定: インターフェース
+    # Proton設定: 互換性 デフォルトでProton Experimental
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+    fontPackages = with pkgs; [ migu ];
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  systemd.services.NetworkManager-wait-online = {
+    serviceConfig = {
+      ExecStart = [ "" "${pkgs.networkmanager}/bin/nm-online -q --timeout=30" ];
+    };
+  };
+  services = {
+    openssh = {
+      enable = true;
+      settings = { PasswordAuthentication = false; };
+    };
+    xremap = {
+      userName = username;
+      serviceMode = "system";
+      config = {
+        virtual_modifiers =
+          [ "KEY_KATAKANAHIRAGANA" "KEY_HENKAN" "KEY_MUHENKAN" ];
+        modmap = [
+          {
+            name = "CL to Ctrl";
+            remap = { CAPSLOCK = "LEFTCTRL"; };
+          }
+          {
+            name = "無変換 to Esc";
+            remap = {
+              KEY_MUHENKAN = {
+                held = "KEY_MUHENKAN";
+                alone = "KEY_ESC";
+              };
+            };
+          }
+        ];
+        keymap = [
+          {
+            name = "<C-h> to <BS>";
+            remap = { Ctrl_L-h = "Backspace"; };
+          }
+          {
+            name = "<C-m> to <CR>";
+            remap = { Ctrl_L-m = "KEY_ENTER"; };
+          }
+          {
+            name = "無変換+J to AltTab";
+            remap = { KEY_MUHENKAN-j = "ALT-TAB"; };
+          }
+          {
+            name = "無変換+G to Ctrl+Tab";
+            remap = {
+              KEY_MUHENKAN-g = "C-TAB";
+              KEY_MUHENKAN-t = "C-SHIFT-TAB";
+            };
+          }
+        ];
+        keypress_delay_ms = 1;
+      };
+    };
+  };
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
---
+++ mypc/flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "NixOS config";
+  inputs = {
+    # nixpkgs.url = "github:nixos/nixpkgs?ref=release-25.05";
+    nixpkgs-pin.url = "path:../pkgs";
+    nixpkgs.follows = "nixpkgs-pin/nixpkgs";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    plasma-manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
+    xremap.url = "github:xremap/nix-flake";
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, nixos-hardware, home-manager, sops-nix, plasma-manager
+    , xremap, nixvim, ... }:
+    let username = "hiroki";
+    in {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {
+          inherit username;
+          inherit nixos-hardware;
+          inherit xremap;
+        };
+        modules = [
+          ./config.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              backupFileExtension = "backup";
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              sharedModules = [
+                nixvim.homeModules.nixvim
+                plasma-manager.homeManagerModules.plasma-manager
+              ];
+              users."${username}" = import ./home.nix username;
+            };
+          }
+          sops-nix.nixosModules.sops
+        ];
+      };
+    };
+}
