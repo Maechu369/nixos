68ffebd63a8774b25b7ab65ad9e434a42640a562
Maechu369 <90904222+Maechu369@users.noreply.github.com>
2025-08-10 18:21:42 +0900

    purge xremap from letsnote

--- letsnote/config.nix
+++ letsnote/config.nix
@@ -193,51 +193,7 @@ in {
       enable = true;
       settings = { PasswordAuthentication = false; };
     };
-    xremap = {
-      userName = username;
-      serviceMode = "system";
-      config = {
-        virtual_modifiers =
-          [ "KEY_KATAKANAHIRAGANA" "KEY_HENKAN" "KEY_MUHENKAN" ];
-        modmap = [
-          {
-            name = "CL to Ctrl";
-            remap = { CAPSLOCK = "LEFTCTRL"; };
-          }
-          {
-            name = "無変換 to Esc";
-            remap = {
-              KEY_MUHENKAN = {
-                held = "KEY_MUHENKAN";
-                alone = "KEY_ESC";
-              };
-            };
-          }
-        ];
-        keymap = [
-          {
-            name = "<C-h> to <BS>";
-            remap = { Ctrl_L-h = "Backspace"; };
-          }
-          {
-            name = "<C-m> to <CR>";
-            remap = { Ctrl_L-m = "KEY_ENTER"; };
-          }
-          {
-            name = "無変換+J to AltTab";
-            remap = { KEY_MUHENKAN-j = "ALT-TAB"; };
-          }
-          {
-            name = "無変換+G to Ctrl+Tab";
-            remap = {
-              KEY_MUHENKAN-g = "C-TAB";
-              KEY_MUHENKAN-t = "C-SHIFT-TAB";
-            };
-          }
-        ];
-        keypress_delay_ms = 1;
-      };
-    };
+    xremap = import ../component/xremap username;
   };
 
   # Open ports in the firewall.
