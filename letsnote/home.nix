9e23200adfe22dc10dac8ad60c6146579b30d7fa
Maechu369 <m6a7e0d8a3@gmail.com>
2025-08-15 18:44:58 +0900

    letsnoteもhomeを分離

--- letsnote/home.nix
+++ letsnote/home.nix
@@ -1,22 +1,8 @@
 username:
 arg@{ config, pkgs, plasma-manager, nixvim, ... }:
-let args = arg // { inherit username; };
-in {
-  home.username = username;
-  home.homeDirectory = "/home/${username}";
-  home.stateVersion = "25.05";
-  home.packages = (import ../component/home/packages.nix args)
-    ++ (import ../component/home/desktop/packages.nix args);
-  xdg.userDirs.enable = true;
-  xdg.configFile = {
-    # "kxkbrc".text = builtins.readFile ./kxkbrc;
-    # "kwinrc".text = builtins.readFile ./kwinrc;
-    fcitx5.source = ./fcitx5;
-    fcitx5.recursive = true;
-    "libskk/rules".source = ./libskk;
-    "libskk/rules".recursive = true;
+let
+  args = arg // {
+    inherit username;
+    desktop = true;
   };
-  programs = (import ../component/home/programs.nix args)
-    // (import ../component/home/desktop/programs.nix args);
-  services = import ../component/home/services args;
-}
+in import ../component/home args
