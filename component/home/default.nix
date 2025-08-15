f3ef87e6dc74494577f85b5eff8a2987f276f3cf
Maechu369 <m6a7e0d8a3@gmail.com>
2025-08-15 18:43:58 +0900

    homeにdesktopか否かの分岐を生成

--- component/home/default.nix
+++ component/home/default.nix
@@ -1,20 +1,20 @@
-args@{ config, pkgs, username, plasma-manager, nixvim, ... }:
-{
+args@{ config, pkgs, username, desktop ? null, plasma-manager, nixvim, ... }: {
   home.username = username;
   home.homeDirectory = "/home/${username}";
   home.stateVersion = "25.05";
   home.packages = (import ./packages.nix args)
-    ++ (import ./desktop/packages.nix args);
+    ++ (if desktop != null then import ./desktop/packages.nix args else [ ]);
   xdg.userDirs.enable = true;
-  xdg.configFile = {
+  xdg.configFile = if desktop != null then {
     # "kxkbrc".text = builtins.readFile ./kxkbrc;
     # "kwinrc".text = builtins.readFile ./kwinrc;
     fcitx5.source = ./fcitx5;
     fcitx5.recursive = true;
     "libskk/rules".source = ./libskk;
     "libskk/rules".recursive = true;
-  };
+  } else
+    { };
   programs = (import ./programs.nix args)
-    // (import ./desktop/programs.nix args);
+    // (if desktop != null then import ./desktop/programs.nix args else { });
   services = import ./services args;
 }
--- mypc/home.nix
+++ mypc/home.nix
@@ -1,5 +1,8 @@
 username:
 arg@{ config, pkgs, plasma-manager, nixvim, ... }:
 let
-  args = arg // { inherit username; };
+  args = arg // {
+    inherit username;
+    desktop = true;
+  };
 in import ../component/home args
