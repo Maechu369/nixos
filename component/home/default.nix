7d7f47a119c5e5f54e03ba9e892a5f6f1e76ef77
Maechu369 <m6a7e0d8a3@gmail.com>
2025-08-16 01:41:46 +0900

    add color.sh

--- component/home/default.nix
+++ component/home/default.nix
@@ -4,14 +4,24 @@ args@{ config, pkgs, username, desktop ? null, plasma-manager, nixvim, ... }: {
   home.stateVersion = "25.05";
   home.packages = (import ./packages.nix args)
     ++ (if desktop != null then import ./desktop/packages.nix args else [ ]);
+  home.file = {
+    "bin" = {
+      source = ./bin;
+      recursive = true;
+    };
+  };
   xdg.userDirs.enable = true;
   xdg.configFile = if desktop != null then {
     # "kxkbrc".text = builtins.readFile ./kxkbrc;
     # "kwinrc".text = builtins.readFile ./kwinrc;
-    fcitx5.source = ./fcitx5;
-    fcitx5.recursive = true;
-    "libskk/rules".source = ./libskk;
-    "libskk/rules".recursive = true;
+    "fcitx5" = {
+      source = ./fcitx5;
+      recursive = true;
+    };
+    "libskk/rules" = {
+      source = ./libskk;
+      recursive = true;
+    };
   } else
     { };
   programs = (import ./programs.nix args)
