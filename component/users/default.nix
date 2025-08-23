91433c96eedb2994b65b3ff69dce0d2881516e37
Maechu369 <m6a7e0d8a3@gmail.com>
2025-08-23 16:57:37 +0900

    set new ssh key to access from my phone

--- component/users/default.nix
+++ component/users/default.nix
@@ -6,6 +6,7 @@
     extraGroups = [ "networkmanager" "wheel" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFbkbVhapmW864se06Wk+IWzm5XmfsP0nohg0MVX9b1i openpgp:0x67DC50BF"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOLR3JlwwfoQ6epSxsqTTCVdN61xZqo3C4YFkxHDQ4eT u0_a242@localhost"
     ];
     shell = pkgs.zsh;
     packages = with pkgs; [ kdePackages.kate ];
