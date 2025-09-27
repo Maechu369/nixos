5ced89b9a2adea6905ade276aa00fa3dcd4232db
Maechu369 <m6a7e0d8a3@gmail.com>
2025-09-28 03:03:03 +0900

    ユーザー環境でxremapを実行

--- mypc/home.nix
+++ mypc/home.nix
@@ -2,7 +2,6 @@
 { config, pkgs, plasma-manager, nixvim, ... }: {
   imports = [
     xremap.homeManagerModules.default
-    # (import ../component/home/desktop/xremap.nix)
     (import ../component/home username)
     (import ../component/home/desktop username)
   ];
