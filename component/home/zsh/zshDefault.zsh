cacb7212be7426640f3df78dbe194532f12b51b3
Maechu369 <m6a7e0d8a3@gmail.com>
2025-10-13 14:17:49 +0900

    fkill to bind

--- component/home/zsh/zshDefault.zsh
+++ component/home/zsh/zshDefault.zsh
@@ -74,13 +74,7 @@ git() {
 alias fd="command fd --type d --strip-cwd-prefix | fzf --prompt='cd > ' --preview='eza --color=always --git --icons -1F {}' --bind 'enter:become(cd {})'"
 
 fkill() {
-  local process
-  local pid
-  process=$(procs --tree | fzf --prompt='kill > ')
-  [[ "$process" == '' ]] && return 1
-  pid=$(echo "$process" | grep -oE '[0-9]+' | head -n 1)
-  kill "$pid"
-  echo "$pid"
+  $(procs --tree | fzf --prompt='kill > ' --bind='enter:become(kill $(echo {} | grep -oE "[0-9]+" | head -n 1))')
 }
 
 sys() {
