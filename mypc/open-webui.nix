04a17992d02d5fea0855452121b928f268315eb7
Maechu369 <m6a7e0d8a3@gmail.com>
2026-02-15 20:00:45 +0900

    set open-webui firewall

--- mypc/open-webui.nix
+++ mypc/open-webui.nix
@@ -5,5 +5,11 @@
     host = "0.0.0.0";
     openFirewall = true;
   };
+  networking.firewall = {
+    extraInputRules = ''
+      ip saddr 100.0.0.0/8 tcp dport 8080 accept
+      ip saddr 192.168.2.0/24 tcp dport 8080 accept
+    '';
+  };
 }
 
