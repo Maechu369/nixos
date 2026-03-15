69f37a5830d2dabbedb3fb0184715168c1ef84f7
Maechu369 <m6a7e0d8a3@gmail.com>
2026-03-15 11:08:45 +0900

    add avante.nvim

--- mypc/nixvim.nix
+++ mypc/nixvim.nix
@@ -1,8 +1,33 @@
 { pkgs, lib, ... }:
 {
   programs.nixvim.plugins = {
+    nui = {
+      enable = true;
+    };
+    dressing = {
+      enable = true;
+    };
     avante = {
       enable = true;
+      settings = {
+        provider = "ollama";
+        auto_suggestion_provider = "ollama";
+        providers = {
+          ollama = {
+            local = true;
+            endpoint = "localhost:11434";
+            extra_request_body = {
+              temperature = 1;
+              max_tokens = 128000;
+            };
+            model = "qwen3.5:4b";
+            is_env_set.__raw = ''require("avante.providers.ollama").check_endpoint_alive'';
+          };
+        };
+        behaviour = {
+          auto_suggestions = true;
+        };
+      };
     };
   };
 }
--- mypc/ollama.nix
+++ mypc/ollama.nix
@@ -1,6 +1,8 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   services.ollama = {
     enable = true;
     package = pkgs.ollama-cuda;
+    loadModels = [ "qwen3.5:4b" ];
   };
 }
