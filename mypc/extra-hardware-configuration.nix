1a094b1a4a502e9ac54149ac55d29c970bfcfee3
Maechu369 <m6a7e0d8a3@gmail.com>
2026-03-21 11:24:13 +0900

    新しいmypc環境へ移動

--- mypc/extra-hardware-configuration.nix
+++ mypc/extra-hardware-configuration.nix
@@ -1,21 +1,4 @@
 { ... }: {
-  fileSystems."/mnt/c" = {
-    device = "/dev/disk/by-uuid/6CE4E756E4E72156";
-    fsType = "ntfs";
-    options = [ "fmask=0022" "dmask=0022" ];
-  };
-
-  fileSystems."/mnt/d" = {
-    device = "/dev/disk/by-uuid/C2440DAC440DA46F";
-    fsType = "ntfs";
-    options = [ "fmask=0022" "dmask=0022" ];
-  };
-
-  swapDevices = [{
-    device = "/var/lib/swapfile";
-    size = 32 * 1024; # MB
-  }];
-
   zramSwap = {
     enable = true;
     priority = 5;
@@ -25,9 +8,9 @@
   boot.tmp.useTmpfs = true;
 
   # swapのあるデバイス
-  boot.resumeDevice = "/dev/disk/by-uuid/f7e01cc4-f1af-47ce-bb2b-14cd7bbd26ac";
+  #boot.resumeDevice = "/dev/disk/by-uuid/f7e01cc4-f1af-47ce-bb2b-14cd7bbd26ac";
   # swapfileのoffsetの先頭(swapfileのみ)
   # sudo filefrag -v /path/to/swapfile | head -n 4
-  boot.kernelParams = [ "resume_offset=427436032" ];
+  #boot.kernelParams = [ "resume_offset=427436032" ];
 }
 
