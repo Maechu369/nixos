c20da13545404978623c62cc8b083cd2e385eb18
Maechu369 <m6a7e0d8a3@gmail.com>
2026-03-21 15:45:51 +0900

    set swap

--- mypc/extra-hardware-configuration.nix
+++ mypc/extra-hardware-configuration.nix
@@ -1,4 +1,8 @@
 { ... }: {
+  swapDevices = [{
+    device = "/dev/vg/swap";
+  }];
+
   zramSwap = {
     enable = true;
     priority = 5;
@@ -8,9 +12,6 @@
   boot.tmp.useTmpfs = true;
 
   # swapのあるデバイス
-  #boot.resumeDevice = "/dev/disk/by-uuid/f7e01cc4-f1af-47ce-bb2b-14cd7bbd26ac";
-  # swapfileのoffsetの先頭(swapfileのみ)
-  # sudo filefrag -v /path/to/swapfile | head -n 4
-  #boot.kernelParams = [ "resume_offset=427436032" ];
+  boot.resumeDevice = "/dev/vg/swap";
 }
 
