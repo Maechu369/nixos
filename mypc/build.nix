c1d56fe268738af3c156026c8499608fd2082b63
Maechu369 <m6a7e0d8a3@gmail.com>
2026-03-22 01:24:26 +0900

    mypcでderivationをroot上で行うように

--- mypc/build.nix
+++ mypc/build.nix
@@ -1,15 +1,6 @@
 { ... }:
 {
-  fileSystems."/nix/build" = {
-    device = "tmpfs";
-    fsType = "tmpfs";
-    options = [
-      "size=16G"
-      "mode=755"
-    ];
-  };
-
   nix.settings = {
-    build-dir = "/nix/build";
+    build-dir = "/build";
   };
 }
