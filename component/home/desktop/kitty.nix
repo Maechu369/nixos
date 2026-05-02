f169cf85f576bc7f3ec834b38ac6aaad1e43d1dc
Maechu369 <m6a7e0d8a3@gmail.com>
2026-05-02 11:39:33 +0900

    kitty設定にnixを活用するように

--- component/home/desktop/kitty.nix
+++ component/home/desktop/kitty.nix
@@ -3,16 +3,18 @@
   programs.kitty = {
     enable = true;
     shellIntegration.mode = "no-cursor";
-    extraConfig = ''
-      text_composition_strategy legacy
-      cursor_shape block
-      scrollbar never
-      remember_window_size no
-      initial_window_width 800
-      initial_window_height 600
-      kitty_mod ctrl+shift
-      map kitty_mod+c copy_to_clipboard
-      map kitty_mod+v paste_from_clipboard
-    '';
+    settings = {
+      text_composition_strategy = "legacy";
+      cursor_shape = "block";
+      scrollbar = "never";
+      remember_window_size = "no";
+      initial_window_width = "800";
+      initial_window_height = "600";
+      kitty_mod = "ctrl+shift";
+    };
+    keybindings = {
+      "kitty_mod+c" = "copy_to_clipboard";
+      "kitty_mod+v" = "paste_from_clipboard";
+    };
   };
 }
