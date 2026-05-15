aea450964cc674b1823259559d4d84377af0b0a5
Maechu369 <m6a7e0d8a3@gmail.com>
2026-05-16 02:23:13 +0900

    update flakes to 5/11, but disabled zen kernel

--- component/kernel.nix
+++ component/kernel.nix
@@ -1,5 +1,6 @@
 { pkgs, config, ... }: {
-  boot.kernelPackages = pkgs.linuxPackages_zen;
+  # boot.kernelPackages = pkgs.linuxPackages_zen;
+  boot.kernelPackages = pkgs.linuxPackages;
   # broken
   # boot.extraModulePackages = with config.boot.kernelPackages; [ rtl8852au ];
 }
