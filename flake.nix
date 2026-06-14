edd5ea2d3222607f7238669fd326e59c78c9f3e3
Maechu369 <m6a7e0d8a3@gmail.com>
2026-06-14 17:48:16 +0900

    use flake-parts

--- flake.nix
+++ flake.nix
@@ -8,6 +8,7 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-parts.url = "github:hercules-ci/flake-parts";
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -28,11 +29,11 @@
   };
 
   outputs =
-    {
-      self,
+    inputs@{
       nixpkgs,
       nixos-hardware,
       home-manager,
+      flake-parts,
       sops-nix,
       plasma-manager,
       xremap,
@@ -42,66 +43,68 @@
     let
       username = "hiroki";
     in
-    {
-      nixosConfigurations.mypc = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit username nixos-hardware xremap; };
-        modules = [
-          ./mypc/config.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              backupFileExtension = "backup";
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              sharedModules = [
-                nixvim.homeModules.nixvim
-                plasma-manager.homeModules.plasma-manager
-                sops-nix.homeManagerModules.sops
-              ];
-              extraSpecialArgs = { inherit username xremap; };
-              users."${username}" = ./mypc/home.nix;
-            };
-          }
-          sops-nix.nixosModules.sops
-        ];
-      };
-      nixosConfigurations.letsnote = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit username nixos-hardware xremap; };
-        modules = [
-          ./letsnote/config.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              backupFileExtension = "backup";
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              sharedModules = [
-                nixvim.homeModules.nixvim
-                plasma-manager.homeModules.plasma-manager
-                sops-nix.homeManagerModules.sops
-              ];
-              extraSpecialArgs = { inherit username xremap; };
-              users."${username}" = ./letsnote/home.nix;
-            };
-          }
-          sops-nix.nixosModules.sops
-        ];
-      };
-      nixosConfigurations.iso = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit username; };
-        modules = [
-          ./iso/configuration.nix
-        ];
-      };
-      nixosConfigurations.minimum = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit username nixos-hardware xremap; };
-        modules = [
-          ./minimum/minimum.nix
-        ];
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      flake.nixosConfigurations = {
+        mypc = inputs.nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit username nixos-hardware xremap; };
+          modules = [
+            ./mypc/config.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                backupFileExtension = "backup";
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                sharedModules = [
+                  nixvim.homeModules.nixvim
+                  plasma-manager.homeModules.plasma-manager
+                  sops-nix.homeManagerModules.sops
+                ];
+                extraSpecialArgs = { inherit username xremap; };
+                users."${username}" = ./mypc/home.nix;
+              };
+            }
+            sops-nix.nixosModules.sops
+          ];
+        };
+        nixosConfigurations.letsnote = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit username nixos-hardware xremap; };
+          modules = [
+            ./letsnote/config.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                backupFileExtension = "backup";
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                sharedModules = [
+                  nixvim.homeModules.nixvim
+                  plasma-manager.homeModules.plasma-manager
+                  sops-nix.homeManagerModules.sops
+                ];
+                extraSpecialArgs = { inherit username xremap; };
+                users."${username}" = ./letsnote/home.nix;
+              };
+            }
+            sops-nix.nixosModules.sops
+          ];
+        };
+        nixosConfigurations.iso = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit username; };
+          modules = [
+            ./iso/configuration.nix
+          ];
+        };
+        nixosConfigurations.minimum = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit username nixos-hardware xremap; };
+          modules = [
+            ./minimum/minimum.nix
+          ];
+        };
       };
     };
 }
