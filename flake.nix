f59eca9a85f681f4f0712b4fde300f997d95dc21
Maechu369 <m6a7e0d8a3@gmail.com>
2026-06-20 16:07:22 +0900

    add git-hooks

--- flake.nix
+++ flake.nix
@@ -29,6 +29,10 @@
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    git-hooks-nix = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -51,6 +55,30 @@
         ./letsnote
         ./iso
         ./minimum
+        inputs.git-hooks-nix.flakeModule
       ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          ...
+        }:
+        {
+          pre-commit = {
+            settings.hooks = {
+              nixfmt.enable = true;
+            };
+          };
+          devShells.default = pkgs.mkShellNoCC {
+            shellHook = ''
+              ${config.pre-commit.shellHook}
+              echo "regenerate .pre-commit-config.yaml"
+              exit
+            '';
+            packages = config.pre-commit.settings.enabledPackages;
+          };
+        };
     };
 }
