61689328ceacc64b75d34b33c2c6281fa91548e8
Maechu369 <m6a7e0d8a3@gmail.com>
2026-07-05 22:28:17 +0900

    add qwen3.6:35b-A3B

--- mypc/llama.nix
+++ mypc/llama.nix
@@ -13,13 +13,31 @@ in
     settings = {
       healthCheckTimeout = 120;
       models = {
+        "qwen3.6:35b-a3b" = {
+          # hf download unsloth/Qwen3.6-35B-A3B-GGUF Qwen3.6-35B-A3B-UD-Q4_K_M.gguf --local-dir .
+          cmd = ''
+            ${llama-server}
+              --port ''${PORT}
+              -m /var/lib/llama/models/Qwen3.6-35B-A3B-UD-Q4_K_M.gguf
+              -ngl 999
+              -ot "blk\.(0|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18)\.ffn_.*_exps\.=CUDA0,exps=CPU" \
+              -c 65536
+              --flash-attn on
+              --cache-type-k q8_0
+              --cache-type-v q8_0
+              --jinja
+              --spec-type ngram-simple --spec-draft-n-max 64
+              --no-webui
+          '';
+          aliases = [ "qwen3.6" ];
+        };
         "qwen3.5:4b" = {
           # hf download unsloth/Qwen3.5-4B-GGUF Qwen3.5-4B-Q4_K_M.gguf --local-dir .
           cmd = ''
             ${llama-server}
               --port ''${PORT}
               -m /var/lib/llama/models/Qwen3.5-4B-Q4_K_M.gguf
-              -ngl 99
+              -ngl 999
               -c 65536
               --flash-attn on
               --cache-type-k q8_0
@@ -36,7 +54,7 @@ in
             ${llama-server}
               --port ''${PORT}
               -m /var/lib/llama/models/qwen2.5-coder-3b-instruct-q4_k_m.gguf
-              -ngl 99
+              -ngl 999
               -c 32768
               --flash-attn on
               --jinja
