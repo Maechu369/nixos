a2aa2f3ef19ca186e78c15ed9174a78f67fee262
Maechu369 <m6a7e0d8a3@gmail.com>
2026-07-11 20:09:51 +0900

    avanteではなくllm.nvimを使用して補完するよう変更。avanteは残す

--- mypc/nixvim.nix
+++ mypc/nixvim.nix
@@ -10,12 +10,6 @@
     avante = {
       enable = true;
       settings = {
-        suggestion = {
-          accept = "<Tab>";
-          dismiss = "<C-]>";
-          next = "<M-]>";
-          prev = "<M-[>";
-        };
         provider = "llama_swap";
         input.provider = "dressing";
         auto_suggestion_provider = "llama_swap";
@@ -23,21 +17,25 @@
           llama_swap = {
             __inherited_from = "openai";
             endpoint = "http://localhost:8080/v1";
-            model = "qwen2.5-coder:14b";
-            extra_request_body = {
-              temperature = 1;
-              max_tokens = 8192;
-            };
+            model = "qwen3.6:35b-a3b";
             api_key_name = "AVANTE_DUMMY";
           };
         };
         behaviour = {
-          auto_suggestions = true;
+          auto_suggestions = false;
         };
       };
     };
+    llm = {
+      enable = true;
+      settings = {
+        backend = "openai";
+        url = "http://localhost:8080/v1";
+        model = "qwen2.5-coder:14b";
+      };
+    };
   };
-  home.sessionVariables = {
-    AVANTE_DUMMY = "dummy";
-  };
+  # home.sessionVariables = {
+  #   AVANTE_DUMMY = "dummy";
+  # };
 }
