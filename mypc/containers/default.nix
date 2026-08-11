a87aa1647dda796b78ae7ba23003e6b02e6f790b
Maechu369 <m6a7e0d8a3@gmail.com>
2026-08-12 02:13:26 +0900

    VMにネットワークを接続

--- mypc/containers/default.nix
+++ mypc/containers/default.nix
@@ -2,13 +2,31 @@
   networking = {
     nat = {
       enable = true;
-      internalInterfaces = [ "ve-*" ];
+      internalInterfaces = [
+        "ve-*"
+        "vm-openclaw"
+      ];
       externalInterface = "enp5s0";
       enableIPv6 = false;
     };
     firewall = {
       filterForward = true;
-      trustedInterfaces = [ "ve-*" ];
+      trustedInterfaces = [
+        "ve-*"
+        "vm-openclaw"
+      ];
+    };
+  };
+  systemd.network = {
+    enable = true;
+    networks = {
+      "vm-openclaw" = {
+        matchConfig.Name = "vm-openclaw";
+        networkConfig = {
+          Address = [ "192.168.65.1/24" ];
+          DHCP = "no";
+        };
+      };
     };
   };
   imports = [
--- openclaw/config.nix
+++ openclaw/config.nix
@@ -1,4 +1,29 @@
-{ ... }: {
+{ ... }:
+let
+  mac = "02:00:00:00:00:01";
+in
+{
   networking.hostName = "openclaw";
+  systemd.network = {
+    enable = true;
+    networks."30-lan0" = {
+      matchConfig.MACAddress = mac;
+      networkConfig = {
+        Address = [ "192.168.65.2/24" ];
+        Gateway = "192.168.65.1";
+        DHCP = "no";
+      };
+    };
+  };
+  microvm = {
+    interfaces = [
+      {
+        type = "tap";
+        id = "vm-openclaw";
+        inherit mac;
+      }
+    ];
+    hypervisor = "qemu";
+  };
   system.stateVersion = "26.11";
 }
